9b51bbbc2224ee4de8c391be5f432d262ff7181b
ahmedabzk <ahmed.abzk@gmail.com>
2022-10-25 11:33:17 +0300

    added routes, controllers

--- controllers/foodController.go
+++ controllers/foodController.go
@@ -1,21 +1,47 @@
 package controllers
 
-import "github.com/gin-gonic/gin"
+import (
+	"context"
+	"net/http"
+	"time"
+
+	"github.com/ahmedabzk/restaurant-management/database"
+	"github.com/ahmedabzk/restaurant-management/models"
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+var foodCollection *mongo.Collection = database.OpenCollection(database.Client, "food")
 
 func GetFoods() gin.HandlerFunc{
-	return func(ctx *gin.Context) {
+	return func(c *gin.Context) {
 
 	}
 }
 
 func GetFood() gin.HandlerFunc{
-	return func(ctx *gin.Context) {
+	return func(c *gin.Context) {
+		// use context.WithTimeout so that the API terminates after the specified time
+		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+		// get the id of the food item we looking for from the Params received
+		foodId := c.Param("food_id")
 
+		var food models.Food
+		// look for the food item from the collection using the foodId
+		err := foodCollection.FindOne(ctx, bson.M{"food_id":foodId}).Decode(&food)
+
+		defer cancel()
+
+		if err != nil{
+			c.JSON(http.StatusInternalServerError, gin.H{"err":"error occured while fetching the food item"})
+		}
+		c.JSON(http.StatusOK, food)
 	}
 }
 
 func CreateFood() gin.HandlerFunc{
-	return func(ctx *gin.Context){
+	return func(c *gin.Context){
 
 	}
 }
@@ -29,7 +55,7 @@ func toFixed(num float64, precision int) float64{
 }
 
 func UpdateFood()gin.HandlerFunc{
-	return func(ctx *gin.Context){
+	return func(c *gin.Context){
 
 	}
 }
