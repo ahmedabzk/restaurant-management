823da6c470ac50507791083dda072bcb7c67d20f
ahmedabzk <ahmed.abzk@gmail.com>
2022-10-26 21:02:14 +0300

    added routes, controllers

--- controllers/invoiceController.go
+++ controllers/invoiceController.go
@@ -1,10 +1,40 @@
 package controllers
 
-import "github.com/gin-gonic/gin"
+import (
+	"context"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/ahmedabzk/restaurant-management/database"
+	"github.com/ahmedabzk/restaurant-management/models"
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+var invoiceCollection *mongo.Collection = database.OpenCollection(database.Client, "invoice")
 
 func GetInvoices() gin.HandlerFunc {
-	return func(ctx *gin.Context) {
+	return func(c *gin.Context) {
+		ctx, cancel := context.WithTimeout(context.Background(), 100*time.Second)
 
+
+		result, err := invoiceCollection.Find(context.TODO(), bson.M{})
+		defer cancel()
+
+		if err != nil{
+			c.JSON(http.StatusInternalServerError, gin.H{"error":err.Error()})
+			return 
+		}
+
+		var allInvoice []bson.M
+
+		if err = result.All(ctx, &allInvoice); err != nil{
+			log.Fatal(err)
+		}
+		defer cancel()
+		c.JSON(http.StatusOK, allInvoice)
 	}
 }
 
