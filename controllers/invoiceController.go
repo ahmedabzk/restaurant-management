20fff959b80dd7f02529c5be9b22a57ebcc97ea3
ahmedabzk <ahmed.abzk@gmail.com>
2022-10-26 21:07:20 +0300

    added routes, controllers

--- controllers/invoiceController.go
+++ controllers/invoiceController.go
@@ -7,11 +7,21 @@ import (
 	"time"
 
 	"github.com/ahmedabzk/restaurant-management/database"
-	"github.com/ahmedabzk/restaurant-management/models"
 	"github.com/gin-gonic/gin"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 )
+
+type InvoiceViewMethod struct {
+	Invoice_id       string
+	Payment_method   string
+	Order_id         string
+	Payment_status   *string
+	Payment_due      interface{}
+	Table_number     interface{}
+	Payment_due_date time.Time
+	Order_details    interface{}
+}
 
 var invoiceCollection *mongo.Collection = database.OpenCollection(database.Client, "invoice")
 
@@ -19,18 +29,17 @@ func GetInvoices() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		ctx, cancel := context.WithTimeout(context.Background(), 100*time.Second)
 
-
 		result, err := invoiceCollection.Find(context.TODO(), bson.M{})
 		defer cancel()
 
-		if err != nil{
-			c.JSON(http.StatusInternalServerError, gin.H{"error":err.Error()})
-			return 
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
 		}
 
 		var allInvoice []bson.M
 
-		if err = result.All(ctx, &allInvoice); err != nil{
+		if err = result.All(ctx, &allInvoice); err != nil {
 			log.Fatal(err)
 		}
 		defer cancel()
@@ -38,20 +47,20 @@ func GetInvoices() gin.HandlerFunc {
 	}
 }
 
-func GetInvoice() gin.HandlerFunc{
+func GetInvoice() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
 
 	}
 }
 
-func CreateInvoices() gin.HandlerFunc{
+func CreateInvoices() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
 
 	}
 }
 
-func UpdateInvoice() gin.HandlerFunc{
+func UpdateInvoice() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
-		
+
 	}
 }
