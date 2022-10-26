4bb6e71e4dff5f28fed39b6f8958dacd85600d58
ahmedabzk <ahmed.abzk@gmail.com>
2022-10-26 21:25:44 +0300

    added routes, controllers

--- controllers/invoiceController.go
+++ controllers/invoiceController.go
@@ -7,12 +7,13 @@ import (
 	"time"
 
 	"github.com/ahmedabzk/restaurant-management/database"
+	"github.com/ahmedabzk/restaurant-management/models"
 	"github.com/gin-gonic/gin"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
-type InvoiceViewMethod struct {
+type InvoiceViewFormat struct {
 	Invoice_id       string
 	Payment_method   string
 	Order_id         string
@@ -48,8 +49,36 @@ func GetInvoices() gin.HandlerFunc {
 }
 
 func GetInvoice() gin.HandlerFunc {
-	return func(ctx *gin.Context) {
+	return func(c *gin.Context) {
+		ctx, cancel := context.WithTimeout(context.Background(), 100*time.Second)
 
+		invoiceId := c.Param("invoice_id")
+		var invoice models.Invoice
+
+		err := invoiceCollection.FindOne(ctx, bson.M{"invoice_id":invoiceId}).Decode(&invoice)
+		defer cancel()
+		if err != nil{
+			c.JSON(http.StatusInternalServerError, gin.H{"error":"error occured while trying to fetch the item"})
+			return 
+		}
+
+		var invoiceView InvoiceViewFormat
+
+		allOrderItems, err := ItemsByOrder(invoice.Order_id)
+		invoiceView.Order_id = invoice.Order_id
+		invoiceView.Payment_due_date = invoice.Payment_due_date
+
+		invoiceView.Payment_method = "null"
+		if invoice.Payment_method != nil{
+			invoiceView.Payment_method = *invoice.Payment_method
+		}
+		invoiceView.Invoice_id = invoice.Invoice_id
+		invoiceView.Payment_status = *&invoice.Payment_status
+		invoiceView.Payment_due = allOrderItems[0]["payment_due"]
+		invoiceView.Table_number = allOrderItems[0]["table_number"]
+		invoiceView.Order_details = allOrderItems[0]["order_items"]
+
+		c.JSON(http.StatusOK, invoiceView)
 	}
 }
 
