bff9cddc33218f9a667acb6409277d996689615a
ahmedabzk <ahmed.abzk@gmail.com>
2022-10-28 17:50:24 +0300

    added routes, controllers

--- controllers/userController.go
+++ controllers/userController.go
@@ -1,6 +1,18 @@
 package controllers
 
-import "github.com/gin-gonic/gin"
+import (
+	"context"
+	"net/http"
+	"time"
+
+	"github.com/ahmedabzk/restaurant-management/database"
+	"github.com/ahmedabzk/restaurant-management/models"
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+var userCollection *mongo.Collection = database.OpenCollection(database.Client, "users")
 
 func GetUsers() gin.HandlerFunc{
 	return func(ctx *gin.Context) {
@@ -10,8 +22,21 @@ func GetUsers() gin.HandlerFunc{
 }
 
 func GetUser() gin.HandlerFunc{
-	return func(ctx *gin.Context){
+	return func(c *gin.Context){
+		ctx, cancel := context.WithTimeout(context.Background(), 100*time.Second)
 
+		userId := c.Param("user_id")
+
+		var user models.User
+
+		err := userCollection.FindOne(ctx, bson.M{"user_id":userId}).Decode(&user)
+
+		if err != nil{
+			c.JSON(http.StatusInternalServerError, gin.H{"error":err.Error()})
+			return 
+		}
+		defer cancel()
+		c.JSON(http.StatusOK, user)
 	}
 }
 
