d028c752abdc35b8a3024444a8828fbc2418b5cd
ahmedabzk <ahmed.abzk@gmail.com>
2022-10-28 22:41:44 +0300

    added routes, controllers

--- controllers/userController.go
+++ controllers/userController.go
@@ -3,6 +3,7 @@ package controllers
 import (
 	"context"
 	"net/http"
+	"strconv"
 	"time"
 
 	"github.com/ahmedabzk/restaurant-management/database"
@@ -14,48 +15,80 @@ import (
 
 var userCollection *mongo.Collection = database.OpenCollection(database.Client, "users")
 
-func GetUsers() gin.HandlerFunc{
-	return func(ctx *gin.Context) {
+func GetUsers() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		ctx, cancel := context.WithTimeout(context.Background(), 100*time.Second)
 
+		recordPerPage, err := strconv.Atoi(c.Query("recordPerPage"))
+
+		if err != nil && recordPerPage < 1 {
+			recordPerPage = 10
+		}
+
+		page, err := strconv.Atoi(c.Query("page"))
+
+		if err != nil && page < 1 {
+			page = 1
+		}
+
+		startIndex := (page - 1) * recordPerPage
+
+		startIndex, err = strconv.Atoi(c.Query("startIndex"))
+
+		matchStage := bson.D{"$match", bson.D{{}}}
+		projectStage := bson.D{
+			{
+				"$project", bson.D{
+					{"_id", 0},
+					{"total_count", 1},
+					{"user_items", bson.D{{"$slice", []interface{}{"$data", startIndex, recordPerPage}}}},
+				}}}
+		result, err := userCollection.Aggregate(ctx, *mongo.Pipeline{
+			matchStage, projectStage})
+
+		if err != nil {
+
+		}
+		defer cancel()
 	}
 
 }
 
-func GetUser() gin.HandlerFunc{
-	return func(c *gin.Context){
+func GetUser() gin.HandlerFunc {
+	return func(c *gin.Context) {
 		ctx, cancel := context.WithTimeout(context.Background(), 100*time.Second)
 
 		userId := c.Param("user_id")
 
 		var user models.User
 
-		err := userCollection.FindOne(ctx, bson.M{"user_id":userId}).Decode(&user)
+		err := userCollection.FindOne(ctx, bson.M{"user_id": userId}).Decode(&user)
 
-		if err != nil{
-			c.JSON(http.StatusInternalServerError, gin.H{"error":err.Error()})
-			return 
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
 		}
 		defer cancel()
 		c.JSON(http.StatusOK, user)
 	}
 }
 
-func Signup() gin.HandlerFunc{
+func Signup() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
 
 	}
 }
 
-func Login() gin.HandlerFunc{
+func Login() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
 
 	}
 }
 
-func HashPassword(password string) string{
+func HashPassword(password string) string {
 
 }
 
-func VerifyPassword(userPassword, providedPassword string) (bool, string){
-	
+func VerifyPassword(userPassword, providedPassword string) (bool, string) {
+
 }
