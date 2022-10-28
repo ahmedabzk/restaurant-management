4dade55ee6eaa7a29ad6745d9023721367a08908
ahmedabzk <ahmed.abzk@gmail.com>
2022-10-28 16:07:06 +0300

    added routes, controllers

--- controllers/orderItemController.go
+++ controllers/orderItemController.go
@@ -68,9 +68,9 @@ func GetOrderItem() gin.HandlerFunc{
 		ctx, cancel := context.WithTimeout(context.Background(), 100*time.Second)
 
 		var orderItem models.OrderItem
-		orderItemId := c.Param("order_id")
+		orderItemId := c.Param("order_item_id")
 
-		err := orderItemCollection.FindOne(ctx, bson.M{"order_id":orderItemId}).Decode(&orderItem)
+		err := orderItemCollection.FindOne(ctx, bson.M{"order_item_id":orderItemId}).Decode(&orderItem)
 
 		defer cancel()
 		if err != nil{
@@ -85,6 +85,49 @@ func GetOrderItem() gin.HandlerFunc{
 func CreateOrderItems() gin.HandlerFunc{
 	return func(c *gin.Context) {
 		ctx, cancel := context.WithTimeout(context.Background(), 100*time.Second)
+
+		var orderItemPack OrderItemPack
+		var order models.Order
+
+		if err := c.BindJSON(&orderItemPack); err != nil{
+			c.JSON(http.StatusBadRequest, gin.H{"error":err.Error()})
+			return 
+		}
+
+		order.Order_date, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
+
+		orderItemsToBeInserted := []interface{}{}
+		order.Table_id = &orderItemPack.Table_id
+		order_id := OrderItemOrderCreator(order)
+
+		for _,orderItem := range orderItemPack.Order_items{
+			orderItem.Order_id = order_id
+
+			validationErr := validate.Struct(orderItem)
+
+			if validationErr != nil{
+				c.JSON(http.StatusBadRequest, gin.H{"error":validationErr.Error()})
+				return 
+			}
+
+			orderItem.ID = primitive.NewObjectID()
+			orderItem.Created_at, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
+			orderItem.Updated_at, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
+			orderItem.Order_item_id = orderItem.ID.Hex()
+
+			var num = toFixed(*orderItem.Unit_price, 2)
+			orderItem.Unit_price = &num
+
+			orderItemsToBeInserted = append(orderItemsToBeInserted, orderItem)
+		}
+
+		result, err :=orderItemCollection.InsertMany(ctx, orderItemsToBeInserted)
+		defer cancel()
+		if err != nil{
+			c.JSON(http.StatusBadRequest, gin.H{"error":err.Error()})
+			return 
+		}
+		c.JSON(http.StatusOK, result)
 	}
 }
 
