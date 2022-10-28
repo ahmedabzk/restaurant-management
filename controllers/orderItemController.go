6704568f95c1c70ce0fdf1fd70bf33a1115e5806
ahmedabzk <ahmed.abzk@gmail.com>
2022-10-28 15:21:44 +0300

    added routes, controllers

--- controllers/orderItemController.go
+++ controllers/orderItemController.go
@@ -1,40 +1,99 @@
 package controllers
 
 import (
+	"context"
+	"net/http"
+	"time"
+
+	"github.com/ahmedabzk/restaurant-management/database"
+	"github.com/ahmedabzk/restaurant-management/models"
 	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-func GetOrderItems() gin.HandlerFunc{
-	return func(ctx *gin.Context) {
+var orderItemCollection *mongo.Collection = database.OpenCollection(database.Client, "orderItem")
 
+func GetOrderItems() gin.HandlerFunc{
+	return func(c *gin.Context) {
+		ctx, cancel := context.WithTimeout(context.Background(), 100*time.Second)
+
+		result, err := orderItemCollection.Find(context.TODO(), bson.M{})
+		defer cancel()
+
+		if err != nil{
+			c.JSON(http.StatusInternalServerError, gin.H{"error":err.Error()})
+		}
+
+		var allOrderItems []bson.M
+
+		err = result.All(ctx, &allOrderItems)
+		if err != nil{
+			c.JSON(http.StatusInternalServerError, gin.H{"error":err.Error()})
+			return 
+		}
+		defer cancel()
+
+		c.JSON(http.StatusOK, allOrderItems)
 	}
 }
 
 func GetOrderItemsByOrder() gin.HandlerFunc{
-	return func(ctx *gin.Context) {
+	return func(c *gin.Context) {
 
 	}
 }
 
 func ItemsByOrder(id string) (OrderItems []primitive.M, err error){
-
+	
 }
 
 func GetOrderItem() gin.HandlerFunc{
-	return func(ctx *gin.Context) {
+	return func(c *gin.Context) {
+		ctx, cancel := context.WithTimeout(context.Background(), 100*time.Second)
+
+		var orderItem models.OrderItem
+		orderItemId := c.Param("order_id")
+
+		err := orderItemCollection.FindOne(ctx, bson.M{"order_id":orderItemId}).Decode(&orderItem)
+
+		defer cancel()
+		if err != nil{
+			c.JSON(http.StatusInternalServerError, gin.H{"error":err.Error()})
+			return 
+		}
+		c.JSON(http.StatusOK, orderItem)
 
 	}
 }
 
 func CreateOrderItems() gin.HandlerFunc{
-	return func(ctx *gin.Context) {
-
+	return func(c *gin.Context) {
+		ctx, cancel := context.WithTimeout(context.Background(), 100*time.Second)
 	}
 }
 
 func UpdateOrderItem() gin.HandlerFunc{
-	return func(ctx *gin.Context) {
-		
+	return func(c *gin.Context) {
+		ctx, cancel := context.WithTimeout(context.Background(), 100*time.Second)
+
+		var orderItem models.OrderItem
+
+		orderItemId := c.Param("order_id")
+
+		if err := c.BindJSON(&orderItem); err != nil{
+			c.JSON(http.StatusInternalServerError, gin.H{"error":err.Error()})
+			return 
+		}
+
+		filter := bson.M{"order_id":orderItemId}
+
+		upset := true
+
+		opt := options.UpdateOptions{
+			Upsert: &upset,
+		}
 	}
 }
