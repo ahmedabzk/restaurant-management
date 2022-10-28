2fd00a943688a7cdbd4826d573a4cc8a26bfa33a
ahmedabzk <ahmed.abzk@gmail.com>
2022-10-28 21:52:29 +0300

    added routes, controllers

--- controllers/orderItemController.go
+++ controllers/orderItemController.go
@@ -60,6 +60,11 @@ func GetOrderItemsByOrder() gin.HandlerFunc{
 }
 
 func ItemsByOrder(id string) (OrderItems []primitive.M, err error){
+	ctx, cancel := context.WithTimeout(context.Background(), 100*time.Second)
+
+	matchStage := bson.D{{"$match", bson.D{{"order_id", id}}}}
+	lookupStage := bson.D{{"$lookup", bson.D{{"from", "food"}, {"localField", "food_id"}, {"foreignField", "food_id"}, {"as", "food"}}}}
+	unwindStage := bson.D{{"$unwind", bson.D{{"path", "$food"}, {"preserveNullAndEmptyArrays", true}}}}
 	
 }
 
